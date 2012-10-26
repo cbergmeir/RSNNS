ec52c6aeabb10467d21ea54a9564a07c76a61106
Vitalie Spinu <spinuvit@gmail.com>
2012-10-26 14:45:24 +0200

    use "df" instead of "iris" for transitory transformations in iris.demo

--- demo/iris.R
+++ demo/iris.R
@@ -7,39 +7,39 @@ setSnnsRSeedValue(seed)
 data(iris)
 
 #shuffle the vector
-iris <- iris[sample(1:nrow(iris),length(1:nrow(iris))),1:ncol(iris)]
+df <- iris[sample(1:nrow(iris),length(1:nrow(iris))),1:ncol(iris)]
 
-irisValues <- iris[,1:4]
-irisTargets <- decodeClassLabels(iris[,5])
-#irisTargets <- decodeClassLabels(iris[,5], valTrue=0.9, valFalse=0.1)
+dfValues <- df[,1:4]
+dfTargets <- decodeClassLabels(df[,5])
+#dfTargets <- decodeClassLabels(df[,5], valTrue=0.9, valFalse=0.1)
 
-iris <- splitForTrainingAndTest(irisValues, irisTargets, ratio=0.15)
+df <- splitForTrainingAndTest(dfValues, dfTargets, ratio=0.15)
 
 #normalize data
-iris <- normTrainingAndTestSet(iris)
+df <- normTrainingAndTestSet(df)
 
-#model <- mlp(iris$inputsTrain, iris$targetsTrain, size=5, learnFunc="Quickprop", learnFuncParams=c(0.1, 2.0, 0.0001, 0.1), 
-#    maxit=50, inputsTest=iris$inputsTest, targetsTest=iris$targetsTest)
+model <- mlp(df$inputsTrain, df$targetsTrain, size=5, learnFunc="Quickprop", learnFuncParams=c(0.1, 2.0, 0.0001, 0.1), 
+             maxit=50, inputsTest=df$inputsTest, targetsTest=df$targetsTest)
 
-model <- mlp(iris$inputsTrain, iris$targetsTrain, size=5, learnFunc="BackpropBatch", learnFuncParams=c(10, 0.1), 
-    maxit=100, inputsTest=iris$inputsTest, targetsTest=iris$targetsTest)
+model <- mlp(df$inputsTrain, df$targetsTrain, size=5, learnFunc="BackpropBatch", learnFuncParams=c(10, 0.1), 
+    maxit=100, inputsTest=df$inputsTest, targetsTest=df$targetsTest)
 
-#model <- mlp(iris$inputsTrain, iris$targetsTrain, size=5, learnFunc="SCG", learnFuncParams=c(0, 0, 0, 0), 
-#    maxit=30, inputsTest=iris$inputsTest, targetsTest=iris$targetsTest)
+#model <- mlp(df$inputsTrain, df$targetsTrain, size=5, learnFunc="SCG", learnFuncParams=c(0, 0, 0, 0), 
+#    maxit=30, inputsTest=df$inputsTest, targetsTest=df$targetsTest)
 
 
-#model <- rbfDDA(iris$inputsTrain, iris$targetsTrain)
+#model <- rbfDDA(df$inputsTrain, df$targetsTrain)
 
-#model <- elman(iris$inputsTrain, iris$targetsTrain, size=5, learnFuncParams=c(0.1), maxit=100, inputsTest=iris$inputsTest, targetsTest=iris$targetsTest)
+#model <- elman(df$inputsTrain, df$targetsTrain, size=5, learnFuncParams=c(0.1), maxit=100, inputsTest=df$inputsTest, targetsTest=df$targetsTest)
 
-#model <- rbf(iris$inputsTrain, iris$targetsTrain, size=40, maxit=200, initFuncParams=c(-4, 4,  0.0,  0.2,  0.04), 
+#model <- rbf(df$inputsTrain, df$targetsTrain, size=40, maxit=200, initFuncParams=c(-4, 4,  0.0,  0.2,  0.04), 
 #             learnFuncParams=c(1e-3, 0, 1e-3, 0.1, 0.8), linOut=FALSE)
 
-#model <- rbf(iris$inputsTrain, iris$targetsTrain, size=40, maxit=600, initFuncParams=c(0, 1,  0.0,  0.2,  0.04), 
+#model <- rbf(df$inputsTrain, df$targetsTrain, size=40, maxit=600, initFuncParams=c(0, 1,  0.0,  0.2,  0.04), 
 #             learnFuncParams=c(1e-5, 0, 1e-5, 0.1, 0.8), linOut=TRUE)
  
 ##experimental..:
-##model <- rbf(iris$inputsTrain, iris$targetsTrain, size=20, maxit=50, initFunc="RBF_Weights_Kohonen",
+##model <- rbf(df$inputsTrain, df$targetsTrain, size=20, maxit=50, initFunc="RBF_Weights_Kohonen",
 ##    initFuncParams=c(50,  0.4,  0), learnFuncParams=c(0.01, 0, 0.01, 0.1, 0.8))
 
 #summary(model)
@@ -48,18 +48,18 @@ par(mfrow=c(2,2))
 
 plotIterativeError(model)
 
-predictions <- predict(model,iris$inputsTest)
+predictions <- predict(model,df$inputsTest)
 
-plotRegressionError(predictions[,2], iris$targetsTest[,2])
+plotRegressionError(predictions[,2], df$targetsTest[,2])
 
-confusionMatrix(iris$targetsTrain,fitted.values(model))
-confusionMatrix(iris$targetsTest,predictions)
+confusionMatrix(df$targetsTrain,fitted.values(model))
+confusionMatrix(df$targetsTest,predictions)
 
-plotROC(fitted.values(model)[,2], iris$targetsTrain[,2])
-plotROC(predictions[,2], iris$targetsTest[,2])
+plotROC(fitted.values(model)[,2], df$targetsTrain[,2])
+plotROC(predictions[,2], df$targetsTest[,2])
 
 #confusion matrix with 402040-method
-confusionMatrix(iris$targetsTrain, encodeClassLabels(fitted.values(model),method="402040", l=0.4, h=0.6))
+confusionMatrix(df$targetsTrain, encodeClassLabels(fitted.values(model),method="402040", l=0.4, h=0.6))
 
 model
 
