3537af9c5be676614abc7ac34d38cfe4805e81d2
Vitalie Spinu <spinuvit@gmail.com>
2012-10-26 16:45:28 +0200

    simplify mlp a bit

--- R/mlp.R
+++ R/mlp.R
@@ -126,47 +126,45 @@ mlp <- function(x, ...) UseMethod("mlp")
 #' #confusion matrix with 402040-method
 #' confusionMatrix(iris$targetsTrain, encodeClassLabels(fitted.values(model),
 #'                                                        method="402040", l=0.4, h=0.6))
-mlp.default <- function(x, y, size=c(5), maxit=100, 
+mlp.default <- function(x, y, size=c(5), maxit=100,  
     initFunc="Randomize_Weights", initFuncParams=c(-0.3, 0.3), 
     learnFunc="Std_Backpropagation", learnFuncParams=c(0.2, 0.0), 
     updateFunc="Topological_Order", updateFuncParams=c(0.0),
     hiddenActFunc="Act_Logistic",
     shufflePatterns=TRUE, linOut=FALSE, inputsTest=NULL, targetsTest=NULL, ...) {
-  
+
+
   x <- as.matrix(x)
   y <- as.matrix(y)
-  
-  checkInput(x,y)
 
-  nInputs <- dim(x)[2L]
-  nOutputs <- dim(y)[2L]
-  
-  snns <- rsnnsObjectFactory(subclass=c("mlp"), nInputs=nInputs, maxit=maxit, 
-      initFunc=initFunc, initFuncParams=initFuncParams, 
-      learnFunc=learnFunc, learnFuncParams=learnFuncParams, 
-      updateFunc=updateFunc, 
-      updateFuncParams=updateFuncParams,
-      shufflePatterns=shufflePatterns, computeIterativeError=TRUE)
+  checkInput(x, y)
+
+  nInputs <- ncol(x)
+  nOutputs <- ncol(y)
+
+
+  snns <- rsnnsObjectFactory(
+    subclass=c("mlp"), nInputs=nInputs, maxit=maxit, 
+    initFunc=initFunc, initFuncParams=initFuncParams, 
+    learnFunc=learnFunc, learnFuncParams=learnFuncParams, 
+    updateFunc=updateFunc, updateFuncParams=updateFuncParams,
+    shufflePatterns=shufflePatterns, computeIterativeError=TRUE)
   
   snns$archParams <- list(size=size)
-  
   snns$snnsObject$setUnitDefaults(0,0,1,0,1,"Act_Logistic","Out_Identity")
   snns$snnsObject$createNet(unitsPerLayer=c(nInputs, size, nOutputs),
                             fullyConnectedFeedForward=TRUE,
                             iNames = colnames(x), oNames = colnames(y))
   
-  if(linOut) {
-    outputActFunc <- "Act_Identity"
-  } else {
-    outputActFunc <- "Act_Logistic"
-  }
+  outputActFunc <- if(linOut)  "Act_Identity" else  "Act_Logistic"
   
   snns$snnsObject$setTTypeUnitsActFunc("UNIT_INPUT", "Act_Identity")
   snns$snnsObject$setTTypeUnitsActFunc("UNIT_HIDDEN", hiddenActFunc)
   snns$snnsObject$setTTypeUnitsActFunc("UNIT_OUTPUT", outputActFunc)
   
+  snns <- train(snns,
+                inputsTrain = x, inputsTest = inputsTest,
+                targetsTrain = y, targetsTest = targetsTest)
   
-  snns <- train(snns, inputsTrain=x, targetsTrain=y, inputsTest=inputsTest, targetsTest=targetsTest)
-
 }
 
